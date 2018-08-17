19cf60ad4d13a830377185f88a253cea4a588d4e
Simone Masiero <simone@duiker101.net>
2018-08-17 10:16:07 +0200

    working countries selection

--- src/components/countries.js
+++ src/components/countries.js
@@ -2,31 +2,45 @@ import React from 'react';
 import './countries.css'
 
 class Country extends React.Component {
+    constructor(props) {
+        super(props);
+        this.changeCountry = props.changeCountry;
+    }
+
     render() {
-        return (<div className="country">{this.props.code}</div>);
+        return (<div onClick={this.onClick}
+                     className={`country ${this.props.current ? "current" : ""}`}>{this.props.code}</div>);
+    }
+
+    onClick = () => {
+        this.changeCountry(this.props.code);
     }
 }
 
 class Countries extends React.Component {
-    constructor() {
-        super();
-        this.state = {expanded: false}
-        this.countries = ["EUW", "EUNE", "NA", "BR"];
-        this.countryItems = this.countries.map((code) =>
-            <Country code={code} key={code}/>
-        )
+    constructor(props) {
+        super(props);
+        this.state = {expanded: false};
     }
 
     render() {
         return (
-            <div className={`countries ${this.state.expanded ? "expanded" : ""}`}>
-                {this.countryItems}
+            <div
+                className={`countries ${this.state.expanded ? "expanded" : ""}`}>
+                {
+                    this.props.countries.map((code) =>
+                        <Country code={code}
+                                 key={code}
+                                 changeCountry={this.props.changeCountry}
+                                 current={code === this.props.currentCountry}/>
+                    )
+                }
             </div>
         );
     }
 
     toggleExpansion = () => {
-        this.setState({expanded: !this.state.expanded})
+        this.setState({expanded: !this.state.expanded});
     }
 }
 
--- src/components/search.js
+++ src/components/search.js
@@ -3,20 +3,33 @@ import './search.css'
 import Countries from "./countries";
 
 class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.countries = ["EUW1", "EUN1", "NA1", "KR", "OC1", "BR1", "JP1", "TR1", "RU", "LA1", "LA2"];
+        this.state = {currentCountry: this.countries[0]}
+    }
+
     render() {
         return (
             <div className="search">
                 <input type="text" placeholder="Player Name"/>
-                <button onClick={this.toggleCountries} className="region">EUW1</button>
-                <Countries ref="countries" expanded={false}/>
+                <button onClick={this.toggleCountries} className="region">{this.state.currentCountry}</button>
+                <Countries ref="countries"
+                           countries={this.countries}
+                           currentCountry={this.state.currentCountry}
+                           changeCountry={this.changeCountry} />
             </div>
         );
     }
 
     toggleCountries = () => {
-        // this.refs.countries.expanded = !this.refs.countries.expanded;
         this.refs.countries.toggleExpansion()
-    }
+    };
+
+    changeCountry = (country) => {
+        this.setState({currentCountry: country});
+        this.toggleCountries();
+    };
 }
 
 export default Search;
