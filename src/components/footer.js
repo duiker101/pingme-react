dd0c561b868e08f4ff1d010a0952678016cf150c
Simone Masiero <simone@duiker101.net>
2018-08-19 12:52:04 +0200

    nav and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,11 @@ import './footer.css'
 
 class Footer extends Component {
     render() {
-        return (<footer>footer</footer>);
+        return (<footer>
+            Ping Me isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone
+            officially involved in producing or managing League of Legends. League of Legends and Riot Games are
+            trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
+        </footer>);
     }
 
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -3,7 +3,12 @@ import './nav.css'
 
 class Nav extends Component {
     render() {
-        return (<nav>nav</nav>);
+        return (<nav>
+            <a href="http://riftkit.net">Home</a>
+            <a href="http://pingme.riftkit.net/">Ping Me</a>
+            <a href="http://map.riftkit.net">LoL Planner</a>
+            <a href="http://riftkit.net/about">About</a>
+        </nav>);
     }
 
 }
